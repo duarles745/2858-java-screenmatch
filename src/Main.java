2fd40fd1ee75233cae1b9cfaac84bc6d00763609
Duarles <88401695+duarles745@users.noreply.github.com>
2025-05-01 13:32:51 -0300

    upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+        int ano;
+        ano=2020;
+        
+
+
     }
 }
